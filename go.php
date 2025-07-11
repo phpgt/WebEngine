467bbf1780ab3f4bb8d9a6658037b23a58606315
Greg Bowler <greg@g105b.com>
2025-07-11 15:21:57 +0100

    tweak: allow dots in paths

--- go.php
+++ go.php
@@ -17,7 +17,7 @@ ini_set("html_errors", "false");
  */
 $uri = urldecode(parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH));
 if(strstr($uri, ".")
-	|| is_file($_SERVER["DOCUMENT_ROOT"] . $uri)) {
+&& is_file($_SERVER["DOCUMENT_ROOT"] . $uri)) {
 	return false;
 }
 

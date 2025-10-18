f3fd611f4ac22987bd9bb83f25f05919a3d4c3cb
Greg Bowler <greg@g105b.com>
2025-10-18 12:36:39 +0100

    test: unit test the debug timer

--- src/Debug/Timer.php
+++ src/Debug/Timer.php
@@ -1,16 +1,32 @@
 <?php
 namespace Gt\WebEngine\Debug;
 
+use Closure;
+
+/**
+ * Debug timer for measuring elapsed time within the request lifecycle.
+ *
+ * Behavior:
+ * - On construction, records the start time.
+ * - Call stop() to record the end time.
+ * - Call getDelta() to retrieve the elapsed time in seconds (float).
+ *
+ * Testability:
+ * - Accepts an optional Closure $timeGetter to supply the current time.
+ * - By default, this calls microtime(true).
+ */
 class Timer {
 	private float $startTime;
 	private float $endTime;
+	private Closure $timeGetter;
 
-	public function __construct() {
-		$this->startTime = microtime(true);
+	public function __construct(?Closure $timeGetter = null) {
+		$this->timeGetter = $timeGetter ?? fn() => microtime(true);
+		$this->startTime = ($this->timeGetter)();
 	}
 
 	public function stop():void {
-		$this->endTime = microtime(true);
+		$this->endTime = ($this->timeGetter)();
 	}
 
 	public function getDelta():float {
---
+++ test/phpunit/Debug/TimerTest.php
@@ -0,0 +1,38 @@
+<?php
+namespace Gt\WebEngine\Test\Debug;
+
+use Gt\WebEngine\Debug\Timer;
+use PHPUnit\Framework\TestCase;
+
+class TimerTest extends TestCase {
+	public function testDeltaWithInjectedTimeGetter():void {
+		$times = [100.0, 102.5];
+		$index = 0;
+		$timeGetter = function() use (&$times, &$index) {
+			$value = $times[$index] ?? end($times);
+			$index++;
+			return $value;
+		};
+
+		$sut = new Timer($timeGetter);
+		$sut->stop();
+		self::assertSame(2.5, $sut->getDelta());
+	}
+
+	public function testStopMultipleTimesUsesLatestEndTime():void {
+		$times = [10.0, 15.0, 20.0];
+		$index = 0;
+		$timeGetter = function() use (&$times, &$index) {
+			$value = $times[$index] ?? end($times);
+			$index++;
+			return $value;
+		};
+
+		$sut = new Timer($timeGetter);
+		$sut->stop();
+		self::assertSame(5.0, $sut->getDelta());
+
+		$sut->stop();
+		self::assertSame(10.0, $sut->getDelta());
+	}
+}

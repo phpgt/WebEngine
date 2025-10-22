212f6ffe50b2c506eacfccd46bdb781c26d425b3
Greg Bowler <greg@g105b.com>
2025-10-22 21:01:51 +0100

    test: view tests

--- src/View/JSONView.php
+++ src/View/JSONView.php
@@ -1,6 +1,10 @@
 <?php
-namespace Gt\WebEngine\View;
+namespace GT\WebEngine\View;
 
+/**
+ * This file is just stubbed out for now, and does not need unit tests writing
+ * for it until the structured object document feature is planned out properly.
+ */
 class JSONView extends BaseView {
 	public function createViewModel():JSONDocument {
 		// TODO: Implement createViewModel() method.

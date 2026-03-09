34e3469c2480d8332a6b5d9c7440abde5496f311
Greg Bowler <greg@g105b.com>
2026-03-09 16:55:28 +0000

    tidy: error page rendering

--- src/Dispatch/Dispatcher.php
+++ src/Dispatch/Dispatcher.php
@@ -226,69 +226,12 @@ class Dispatcher {
 			$errorStatusCode = $throwable->getHttpCode();
 		}
 
-		$this->processResponse(true);
-
-// TODO: Why can't I load the Binder here?
-		if($this->viewModel instanceof HTMLDocument) {
-			$trace = $throwable->getTrace();
-			array_unshift($trace, [
-				"file" => $throwable->getFile(),
-				"line" => $throwable->getLine(),
-				"class" => get_class($throwable) . "(\"" . $throwable->getMessage() . "\")",
-			]);
-			foreach($trace as $i => $traceItem) {
-				if(isset($traceItem["file"])) {
-					$cwd = getcwd() . DIRECTORY_SEPARATOR;
-					if(str_starts_with($traceItem["file"], $cwd)) {
-						$trace[$i]["file"] = substr($traceItem["file"], strlen($cwd));
-					}
-				}
-
-				if(isset($traceItem["file"]) && str_starts_with($traceItem["file"], "gt-logic-stream://")) {
-					$trace = array_slice($trace, 0, $i + 1);
-					break;
-				}
-			}
-
-			$binder = $this->serviceContainer->get(Binder::class);
-
-//			($this->viewModelInitCallback)();
-			$binder->bindValue($throwable->getMessage());
-
-			if(!$this->config->getBool("app.production")) {
-				$traceString = "";
-				foreach($trace as $i => $traceItem) {
-					$traceString .= "#$i ";
-					if(isset($traceItem["class"])) {
-						$traceString .= $traceItem["class"];
-						if(isset($traceItem["function"])) {
-							$traceString .= "::";
-							$traceString .= $traceItem["function"];
-						}
-
-						$traceString .= " -> ";
-					}
-
-					if(isset($traceItem["file"])) {
-						$traceString .= $traceItem["file"];
-					}
-					if(isset($traceItem["line"])) {
-						$traceString .= "(" . $traceItem["line"] . ")";
-					}
-					$traceString .= "\n";
-				}
-				$binder->bindKeyValue("trace", $traceString);
-			}
-
-		}
-
-		$this->response = $this->response->withStatus($errorStatusCode);
-
 		if(!$this->viewAssembly->containsDistinctFile()) {
 			throw new ErrorPageNotFoundException($errorStatusCode);
 		}
 
-		$this->processResponse(true);
+		$this->processResponse(true, $throwable);
+		$this->response = $this->response->withStatus($errorStatusCode);
 		return $this->response;
 	}
 
@@ -409,7 +352,10 @@ class Dispatcher {
 	/**
 	 * @return void
 	 */
-	public function processResponse(bool $processingError = false):void {
+	public function processResponse(
+		bool $processingError = false,
+		?Throwable $errorThrowable = null,
+	):void {
 		$dynamicPath = $this->serviceContainer->get(DynamicPath::class);
 
 		$this->viewModelProcessor->processDynamicPath(
@@ -423,6 +369,9 @@ class Dispatcher {
 
 // TODO: CSRF handling - needs to be done on any POST request.
 		($this->viewModelInitCallback)();
+		if($processingError && $errorThrowable) {
+			$this->bindErrorDetails($errorThrowable);
+		}
 
 		foreach($logicAssemblyComponentList as $logicAssemblyComponent) {
 			$assembly = $logicAssemblyComponent->assembly;
@@ -466,5 +415,52 @@ class Dispatcher {
 		$documentBinder->cleanupDocument();
 
 		$this->viewStreamer->stream($this->view, $this->viewModel);
+	}
+
+	private function bindErrorDetails(Throwable $throwable):void {
+		$trace = $throwable->getTrace();
+		array_unshift($trace, [
+			"file" => $throwable->getFile(),
+			"line" => $throwable->getLine(),
+			"class" => get_class($throwable) . "(\"" . $throwable->getMessage() . "\")",
+		]);
+		foreach($trace as $i => $traceItem) {
+			if(isset($traceItem["file"])) {
+				$cwd = getcwd() . DIRECTORY_SEPARATOR;
+				if(str_starts_with($traceItem["file"], $cwd)) {
+					$trace[$i]["file"] = substr($traceItem["file"], strlen($cwd));
+				}
+			}
+
+			if(isset($traceItem["file"]) && str_starts_with($traceItem["file"], "gt-logic-stream://")) {
+				$trace = array_slice($trace, 0, $i + 1);
+				break;
+			}
+		}
+
+		$binder = $this->serviceContainer->get(Binder::class);
+		$binder->bindValue($throwable->getMessage());
+		if(!$this->config->getBool("app.production")) {
+			$traceString = "";
+			foreach($trace as $i => $traceItem) {
+				$traceString .= "#$i ";
+				if(isset($traceItem["class"])) {
+					$traceString .= $traceItem["class"];
+					if(isset($traceItem["function"])) {
+						$traceString .= "::";
+						$traceString .= $traceItem["function"];
+					}
+					$traceString .= " -> ";
+				}
+				if(isset($traceItem["file"])) {
+					$traceString .= $traceItem["file"];
+				}
+				if(isset($traceItem["line"])) {
+					$traceString .= "(" . $traceItem["line"] . ")";
+				}
+				$traceString .= "\n";
+			}
+			$binder->bindKeyValue("trace", $traceString);
+		}
 	}
 }
